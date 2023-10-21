813dd864aa3c3b3ac4c518cfd996c6f01cacf329
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2023-10-22 02:26:02 +1100

    Create 16x16 grid of divs
    Make the squares of the grid and add flex to container

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("#container");
+
+//create 16 x 16 divs
+const NUMBER_OF_SQUARES = 256;
+
+for(let i=0; i<NUMBER_OF_SQUARES; i++){
+  const square = document.createElement("div");
+  square.style.backgroundColor = "blue";
+  //square.style.margin = "3px";
+  square.style.height = "50px";
+  square.style.width = "50px";
+  container.appendChild(square);
+}
